b7bdcbb4f2462612a563e6e6df8ff7b4fb6c2692
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-11-25 11:43:46 +0200

    logout and current route

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const validateBody = require("../../middlewares/validateBody");
 const { schemaRegister } = require("../../schemas/JoiValidator");
 const { ctrlWrapper } = require("../../services");
 const ctrl = require("../../controllers/users");
+const authentication = require("../../middlewares/authentication");
 
 const router = express.Router();
 
@@ -13,4 +14,7 @@ router.post(
 );
 
 router.post("/login", validateBody(schemaRegister), ctrlWrapper(ctrl.login));
+router.get("/logout", authentication, ctrlWrapper(ctrl.logout));
+router.get("/current", authentication, ctrlWrapper(ctrl.current));
+
 module.exports = router;
